0edbc86ac865701f37a54e822132e4824b8a8a06
Moses Erhinyodavwe <moseserhinyodavwe2@gmail.com>
2024-05-31 14:51:50 +0100

    removed the nav and pushed

--- src/app/components/navigator/navigator.js
+++ src/app/components/navigator/navigator.js
@@ -1,27 +1,35 @@
 "use client"
 import { House, Person, Settings, Task } from '@mui/icons-material'
+import { usePathname } from 'next/navigation'
 import React from 'react'
 
 export default function NavigatorComp() {
+    let path = usePathname();
+    console.log(path);
     return (
-        <div className='fixed w-full    bottom-4 z-50'>
+        <>
+            {path == '/' ? <></> : path == '/login' ? <></> :
+                <div className='fixed w-full    bottom-4 z-50'>
 
-            <div className='flex bg-[#FF8A00] text-white m-auto flex-row justify-around w-10/12 rounded-2xl items-center p-2 py-3 '>
-                <div>
-                    <House />
-                </div>
-                <div>
-                    <Task />
-                </div>
-                <div>
-                    <Person />
-                </div>
-                <div>
-                    <Settings />
-                </div>
+                    <div className='flex bg-[#FF8A00] text-white m-auto flex-row justify-around w-10/12 rounded-2xl items-center p-2 py-3 '>
+                        <div>
+                            <House />
+                        </div>
+                        <div>
+                            <Task />
+                        </div>
+                        <div>
+                            <Person />
+                        </div>
+                        <div>
+                            <Settings />
+                        </div>
 
-            </div>
+                    </div>
 
-        </div>
+                </div>
+            }
+        </>
+
     )
 }
