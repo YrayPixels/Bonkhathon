c9b4a2146b3e012c4509d8234e432c655c0e5df4
Moses Erhinyodavwe <moseserhinyodavwe2@gmail.com>
2024-06-10 08:52:49 +0100

    pushed the servce worker for a pwa

--- src/app/meta-data.js
+++ src/app/meta-data.js
@@ -1,9 +1,41 @@
-import { Metadata } from "next";
+import { Metadata, Viewport } from "next";
 
+const APP_NAME = "ChoreChamp";
+const APP_DEFAULT_TITLE = "ChoreChamp - Smart Parenting App";
+const APP_TITLE_TEMPLATE = "%s - PWA App";
+const APP_DESCRIPTION = 'ChoreChamp is a parent children application that helps parents train their children in finacial literacy by rewarding them for tasks done';
 export const metadata = {
-    title: 'Giggles',
-    description: 'Giggles is a parent children application that helps parents train their children in finacial literacy by rewarding them for tasks done',
+    title: { default: APP_NAME, template: '%s - ChoreChamp' },
+
+    description: APP_DESCRIPTION,
     icons: {
         icon: '/images/favicon-32x32.png',
+    },
+    manifest: "/manifest.json",
+    appleWebApp: {
+        capable: true,
+        statusBarStyle: "default",
+        title: APP_DEFAULT_TITLE,
+        // startUpImage: [],
+    },
+    formatDetection: {
+        telephone: false,
+    },
+    openGraph: {
+        type: "website",
+        siteName: APP_NAME,
+        title: {
+            default: APP_DEFAULT_TITLE,
+            template: APP_TITLE_TEMPLATE,
+        },
+        description: APP_DESCRIPTION,
+    },
+    twitter: {
+        card: "summary",
+        title: {
+            default: APP_DEFAULT_TITLE,
+            template: APP_TITLE_TEMPLATE,
+        },
+        description: APP_DESCRIPTION,
     },
 };
