f93a5a1bf716569db3d51a5efe4cff38e6f64495
Moses Erhinyodavwe <moseserhinyodavwe2@gmail.com>
2024-06-10 22:17:17 +0100

    reverted the congif

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,4 @@
-import {
-    PHASE_DEVELOPMENT_SERVER,
-    PHASE_PRODUCTION_BUILD,
-} from "next/constants.js";
-
-/** @type {import("next").NextConfig} */
+/** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
     experimental: {
@@ -11,14 +6,4 @@ const nextConfig = {
     },
 };
 
-const nextConfigFunction = async (phase) => {
-    if (phase === PHASE_DEVELOPMENT_SERVER || phase === PHASE_PRODUCTION_BUILD) {
-        const withPWA = (await import("@next-pwa")).default({
-            dest: "public",
-        });
-        return withPWA(nextConfig);
-    }
-    return nextConfig;
-};
-
-export default nextConfigFunction;
+export default nextConfig;
